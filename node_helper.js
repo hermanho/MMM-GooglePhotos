4ae8c7fc662e8ffd707ceb027c4db190c4a663fa
eouia <eouia0819@gmail.com>
2018-07-27 18:20:01 +0200

    fix the issue about photos shared from others

--- node_helper.js
+++ node_helper.js
@@ -26,6 +26,9 @@ module.exports = NodeHelper.create({
   },
 
   initializeAfterLoading: function (config) {
+    if (config.scanInterval > 1000 * 60 * 60) {
+      config.scanInterval = 1000 * 60 * 55 // By using baseUrl from :search directly, there needs to maintain scanInterval under 1hour because risk of expiration of baseUrl.
+    }
     this.config = config
     console.log(this.name + " initialized after loading.")
     this.authConfig = {
@@ -85,10 +88,12 @@ module.exports = NodeHelper.create({
           found = mediaItems.length
         }
         for (var i in mediaItems) {
+          //console.log("scanned:", mediaItems[i])
           if ("photo" in mediaItems[i].mediaMetadata) {
             var item = {
               "id": mediaItems[i].id,
               "creationTime": Date.parse(mediaItems[i].mediaMetadata.creationTime),
+              "baseUrl": mediaItems[i].baseUrl
             }
             self.tempItems.push(item)
           }
@@ -136,6 +141,9 @@ module.exports = NodeHelper.create({
     }
   },
 
+/* Changed for shared Image.
+ * Very weird API. I don't know why "mediaItems:search" can access shared photos but "mediaItems/[photoID]" cannot.
+ * anyway, here will be trick.
   getPhoto: function() {
     var photoId = this.items[this.index].id
     const options = {
@@ -155,6 +163,7 @@ module.exports = NodeHelper.create({
       if (this.index >= this.items.length) {
         this.index = 0
       }
+      console.log("body", body)
       var payload = {
         "id":body.id,
         "url":body.baseUrl + "=w" + this.config.originalHeightPx + "-h" + this.config.originalHeightPx,
@@ -164,6 +173,31 @@ module.exports = NodeHelper.create({
       this.sendSocketNotification("NEW_IMAGE", payload)
     })
   },
+*/
+  getPhoto: function() {
+    if (this.items.length <= 0) {
+      console.log("There is no scanned photo currently.")
+      return
+    }
+    var photo = null
+    if (typeof this.items[this.index] !== "undefined") {
+      photo = this.items[this.index]
+    } else {
+      photo = this.items[0]
+      this.index = 0
+    }
+    //console.log("photo", this.index, this.items.id)
+    this.index++
+
+    var payload = {
+      "id":photo.id,
+      "url":photo.baseUrl + "=w" + this.config.originalHeightPx + "-h" + this.config.originalHeightPx,
+      "time": Date.parse(photo.creationTime),
+    }
+    this.sendSocketNotification("NEW_IMAGE", payload)
+  },
+
+
 
   broadcast: function() {
     var auth = new Auth(this.authConfig)
