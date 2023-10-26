1cd3ea3bf88a1d4d2b8714f854d6ffced701392d
Herman Ho <hermanho@users.noreply.github.com>
2023-10-26 23:53:21 +0100

    add jsdoc type

---
+++ type.d.ts
@@ -0,0 +1,21 @@
+interface MediaItem {
+  id: string;
+  productUrl: string;
+  baseUrl: string;
+  mimeType: string;
+  mediaMetadata: {
+    creationTime: string;
+    width: string;
+    height: string;
+    photo: {
+      cameraMake: string;
+      cameraModel: string;
+      focalLength: number;
+      apertureFNumber: number;
+      isoEquivalent: number;
+      exposureTime: string;
+    };
+  };
+  filename: string;
+  _albumId: string;
+}
